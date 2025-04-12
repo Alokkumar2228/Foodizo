0a288cfb9abd9e01fca25dc7d28b2e36c00f86ee
Alokkumar2228 <141926450+Alokkumar2228@users.noreply.github.com>
2025-04-13 03:46:47 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -13,7 +13,7 @@ const StoreContextProvider = (props) =>{
     const [token,setToken] = useState(localStorage.getItem("token") || "");
     const[food_list,setFoodList] = useState([]);
 
-    const url = "http://localhost:4000";
+    const url = "https://foodizo-backend-eq3w.onrender.com";
     
 
     const addToCart = async (itemId) => {
